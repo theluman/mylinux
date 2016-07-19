aadf258893e0b4961beb4c7060bfeb96558943a8
theluman <435904632@qq.com>
2016-07-19 12:45:03 +0800

    sd

---
+++ mysql_DML.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+数据库操作语言DML,insert into,delete,update,select
+用到的数据库及表格
+insert into
+    insert into TB_name (COL_Name1,COL_name2,...) values (value1,value2,...);
+delete
+update
+select
+        单表查询（简单查询）
+        多表查询
+        子查询（嵌套查询）
+    子句
+        from子句
+        where子句
+            布尔关系
+            逻辑关系
+        like,rlike子句
+        in
+        is ‘null‘,is not ’null‘
+    算数运算
+    order by排序
+    limit
+    group by分组
+        过滤
+    字段别名
+'''
